aafb78ceefc07bdcdb99fa1d8be08c2d1c19e9dc
slayanin2003 <95961249+slayanin2003@users.noreply.github.com>
2024-05-22 20:07:57 +0500

    Мосунов РИ-311055

---
+++ lab_12/src/pages/About Us.js
@@ -0,0 +1,5 @@
+import React from 'react'; 
+
+const About_Us = () => <h2>О нас</h2>; 
+
+export default About_Us; 
---
+++ lab_12/src/pages/Services.js
@@ -0,0 +1,5 @@
+import React from 'react'; 
+
+const Services = () => <h2>Услуги</h2>; 
+
+export default Services; 
